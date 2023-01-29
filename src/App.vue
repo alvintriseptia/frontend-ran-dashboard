768db70f6e74a99e4a8401a309e0ab866ffb273f
alvintriseptia <alvintryseptia@gmail.com>
2023-01-29 19:27:44 +0700

    add marginY

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 			<Sidebar />
 			<div class="text-transparent">1</div>
 		</el-col>
-		<el-col :span="21" class="px-4">
+		<el-col :span="21" class="px-4 my-2">
 			<h1 class="text-3xl font-bold mb-8">
 				Dashboard Squad Operation & Availability 2023
 			</h1>
